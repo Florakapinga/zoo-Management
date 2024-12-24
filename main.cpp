baf3d6daf935480e2218212832193bd38590bb5a
kapinga <erickfloriana@gmail.com>
2024-12-24 12:01:01 -0800

    animal class

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <string>
+class Animal
+{
+public:
+    Animal(std::string name, std::string species) : name(name), species(species) {}
+    virtual void displayInfo()
+    {
+        std::cout << "Name: " << name << ", Species: " << species << std::endl;
+    }
